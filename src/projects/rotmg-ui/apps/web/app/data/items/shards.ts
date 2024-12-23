c832f723a433dcf154f2a02a1ef2f227f62c5131
Zougui <zougui.dev@protonmail.com>
2024-12-23 13:10:35 +0100

    fixed shards in hoard

--- src/projects/rotmg-ui/apps/web/app/data/items/shards.ts
+++ src/projects/rotmg-ui/apps/web/app/data/items/shards.ts
@@ -1,27 +1,27 @@
 import { ItemIcons } from '~/components/icons';
 
-export const ShardOfTheMadGod = {
+export const shardOfTheMadGod = {
   id: 'shards.shardOfTheMadGod',
   name: 'Shard Of The Mad God',
   Icon: ItemIcons.oryxShards.ShardOfTheMadGod,
   count: 25,
 };
 
-export const ShardOfTheIntern = {
+export const shardOfTheIntern = {
   id: 'shards.shardOfTheIntern',
   name: 'Shard Of The Intern',
   Icon: ItemIcons.oryxShards.ShardOfTheIntern,
   count: 15,
 };
 
-export const ShardOfTheDoorwarden = {
+export const shardOfTheDoorwarden = {
   id: 'shards.shardOfTheDoorwarden',
   name: 'Shard Of The Doorwarden',
   Icon: ItemIcons.oryxShards.ShardOfTheDoorwarden,
   count: 35,
 };
 
-export const ShardOfTheAdvisor = {
+export const shardOfTheAdvisor = {
   id: 'shards.shardOfTheAdvisor',
   name: 'Shard Of The Advisor',
   Icon: ItemIcons.oryxShards.ShardOfTheAdvisor,
